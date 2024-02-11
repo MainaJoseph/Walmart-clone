6284ef93641127f4b3547d83ed4871719ad65f97
MainaJoseph <mainajm254@gmail.com>
2024-02-11 17:26:01 +0300

    Added a loder to show when scrapping data from walmart api with oxylab

---
+++ app/search/loading.tsx
@@ -0,0 +1,23 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+function Loading() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-2">Scrapping Results</h1>
+      <h2 className="mb-5 text-gray-400">We wont be long...</h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-18">
+        <Skeleton className="w-[300px] h-[400px]" />
+        <Skeleton className="w-[300px] h-[400px]" />
+        <Skeleton className="w-[300px] h-[400px]" />
+        <Skeleton className="w-[300px] h-[400px]" />
+        <Skeleton className="w-[300px] h-[400px]" />
+        <Skeleton className="w-[300px] h-[400px]" />
+        <Skeleton className="w-[300px] h-[400px]" />
+        <Skeleton className="w-[300px] h-[400px]" />
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
