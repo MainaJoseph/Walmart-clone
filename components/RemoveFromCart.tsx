90a5288daf1dbb02dc08ace2ba1d8b802df56429
MainaJoseph <mainajm254@gmail.com>
2024-02-12 01:23:17 +0300

    Added RemoveFromCart.tsx

--- components/RemoveFromCart.tsx
+++ components/RemoveFromCart.tsx
@@ -2,10 +2,21 @@
 
 import { useCartStore } from "@/store";
 import { Product } from "@/typings/productTypings";
+import { Button } from "./ui/button";
 
 function RemoveFromCart({ product }: { product: Product }) {
-  const removeFromCart = useCartStore((state) => [state.removeFromCart]);
-  return <div></div>;
+  const removeFromCart = useCartStore((state) => state.removeFromCart);
+
+  const handleRemove = () => {
+    console.log("Remove from cart", product.meta.sku);
+    removeFromCart(product);
+  };
+
+  return (
+    <Button className="bg-walmart hover:bg-walmart/50" onClick={handleRemove}>
+      -
+    </Button>
+  );
 }
 
 export default RemoveFromCart;
