123f05b387f9dae6985684acf79225d8320c8f17
MainaJoseph <mainajm254@gmail.com>
2024-02-11 04:06:25 +0300

    Fixed mobile responsiveness

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+
 import Link from "next/link";
 import {
   Search,
@@ -11,8 +12,8 @@ import {
 
 function Header() {
   return (
-    <header className="flex bg-walmart px-10 py-7 space-x-5">
-      <Link href="/">
+    <header className="flex flex-col md:flex-row bg-walmart px-10 items-center py-7 space-x-5">
+      <Link href="/" className="mb-5 md:mb-0">
         <Image
           src="https://links.papareact.com/yur"
           alt="Home"
@@ -31,6 +32,56 @@ function Header() {
           <Search className="rounded-full h-10 px-2 w-10 bg-yellow-400 cursor-pointer" />
         </button>
       </form>
+      <div className="flex space-x-5 mt-5 md:mt-0">
+        <Link
+          href={"/"}
+          className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <Grid2X2 size={20} />
+          <p>Departments</p>
+        </Link>
+
+        <Link
+          href={"/"}
+          className="hidden xl:flex text-white font-bold items-center space-x-2 text-sm"
+        >
+          <LayoutGrid size={20} />
+          <p>Services</p>
+        </Link>
+
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center text-sm"
+        >
+          <Heart size={20} />
+          <div className="ml-2">
+            <p className="text-xs font-extralight">Reorder</p>
+            <p>My Items</p>
+          </div>
+        </Link>
+
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center text-sm"
+        >
+          <User size={20} />
+          <div className="ml-2">
+            <p className="text-xs font-extralight">Sign In</p>
+            <p>Account</p>
+          </div>
+        </Link>
+
+        <Link
+          href={"/"}
+          className="flex text-white font-bold items-center text-sm"
+        >
+          <ShoppingCart size={20} />
+          <div className="ml-2">
+            <p className="text-xs font-extralight">No Items</p>
+            <p>$0.00</p>
+          </div>
+        </Link>
+      </div>
     </header>
   );
 }
