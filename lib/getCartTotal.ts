0211f9b74c015cb1a91f18ee3e396fd42a1fc3af
MainaJoseph <mainajm254@gmail.com>
2024-02-12 02:19:01 +0300

    Fixed Undefined currency if price === 0

--- lib/getCartTotal.ts
+++ lib/getCartTotal.ts
@@ -1,12 +1,21 @@
 import { Product } from "@/typings/productTypings";
 
 export function getCartTotal(products: Product[]): string {
+  if (products.length === 0) {
+    return "USD 0.00";
+  }
+
+  const currency = products[0]?.currency; // Get currency from the first product
+  if (!currency) {
+    return "Currency undefined";
+  }
+
   const total = products.reduce(
     (acc: number, currentProduct: Product) => acc + currentProduct.price,
     0
   );
 
-  return `${products[0]?.currency} ${total.toFixed(2)}`;
+  return `${currency} ${total.toFixed(2)}`;
 }
 
 export default getCartTotal;
